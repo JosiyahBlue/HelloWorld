26f0349e64c26341c91604118fad3375f83b39c1
JosiyahBlue <josiyahb@nycstudents.net>
2025-09-09 11:24:56 -0400

    added

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("What's your name?");
         Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");J
     }
 }
